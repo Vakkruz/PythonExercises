4873d493feb93a50b950f9b96f0dd34dee8a4d93
Von <jw299@njit.edu>
2019-05-19 15:05:10 -0400

    exercise1 done

--- Example1.py
+++ Example1.py
@@ -1,3 +1,9 @@
-name = str(input("What's your name?\n"))
-age = int(input("What's your age?\n"))
-print(name+", you are "+"year(s) old.")
+name = str(input("What's your name? "))
+age = int(input("What's your age? "))
+print("\n")
+
+year = 2019
+yr_to_age_diff = 100 - age
+cent_year = 2019 + yr_to_age_diff
+
+print(name+", you will be 100 years old in the year of " + str(cent_year) +".")
